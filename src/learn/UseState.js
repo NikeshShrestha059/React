d1564420b54c6734b23a0cbc02d07a4ffdfb6ee9
Nikesh Shrestha <nikupiku059@gmail.com>
2026-03-09 21:44:38 +0545

    starting hook(useState)

---
+++ src/learn/UseState.js
@@ -0,0 +1,26 @@
+// import React, {useState} from 'react';
+// function Counter(){
+//   const [count, setCount]= useState(0);
+//    return(
+//     <div>
+//       <p>You chicked {count} time</p>
+//       <button onClick={()=> setCount(count + 1)}>Click me</button>
+//     </div>
+//    );
+// }
+// import { useState } from "react";
+
+// function ToggleText() {
+//   const [show, setShow] = useState(false);
+
+//   return (
+//     <div>
+//       <button onClick={() => setShow(!show)}>
+//         Toggle Text
+//       </button>
+
+//       {show && <p>Hello World</p>}
+//     </div>
+//   );
+// }
+// export default ToggleText;

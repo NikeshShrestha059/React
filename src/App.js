179ed7541676b84257f021218fdafeba7ac48848
Nikesh Shrestha <nikupiku059@gmail.com>
2026-03-16 14:28:21 +0545

    map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const fruits = ['Apple','Banana','Orange'];
+/* const fruits = ['Apple','Banana','Orange'];
 function FruitList(){
     return(
         <ul>
@@ -8,3 +8,58 @@ function FruitList(){
     );
 }
 export default FruitList;
+
+const Fish = ['Oscar','Channa','Betta'];
+function Fishes(){
+    return(
+        <ul>
+            {Fish.map((item,index) =>
+              <li key={index}>{item}</li>)}
+        </ul>
+
+    )
+}
+export default Fishes;
+
+const Fish = ['Oscar','Channa','Betta'];
+function Fishes(){
+    return(
+        <ul>
+            {Fish.map((item,index) =>
+              <li key={index}>{index}={item}</li>)}
+        </ul>
+
+    )
+}
+export default Fishes;
+
+const number=[1,2,3,4,5,6,7,8,9];
+function Add(){
+    return(
+        <ul>
+             {number.map((item,index)=>(
+
+        
+        <li key ={index}>{index}:{index + index}</li>
+             ))}
+        </ul>
+       );
+}
+export default Add;*/
+
+const number=[1,2,3,4,5,6,7,8,9];
+function Add(){
+    return(
+        <ul>
+            {number.map((item, index, array)=>(
+            <li key={index}>
+                Current Index: {index} +
+                New Index: {index + 1}=
+                {index + (index + 1)}
+            </li>
+        ))}
+        </ul>
+    );
+}
+
+export default Add;
